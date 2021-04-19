b870bc9fad726402e318a1ca4cb51f5356503425
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-19 16:50:32 +0200

    Register Navbar.

--- src/layouts/Admin/Admin.js
+++ src/layouts/Admin/Admin.js
@@ -1,11 +1,11 @@
-// import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import sidenavComponent from '@/components/Sidenav'
+import navbarComponent from '@/components/Navbar'
 
 export default {
     name: 'AdminLayout',
-    components: { sidenavComponent },
+    components: { sidenavComponent, navbarComponent },
     mounted() {
         document.body.classList.add('g-sidenav-show', 'bg-gray-100')
     }
