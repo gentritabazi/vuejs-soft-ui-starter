06f07aa19be6c85f1648c12f0232765f45fa9971
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-19 15:53:22 +0200

    Create Navbar Component.

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'Sidenav',
+    data() {
+        return {
+            breadCrumbList: [],
+            breadCrumbTotal: 0
+        }
+    },
+    mounted() { this.updateList() },
+    watch: { '$route'() { this.updateList() } },
+    methods: {
+        updateList() {
+            this.breadCrumbList = this.$route.meta.breadCrumb
+            this.breadCrumbTotal = this.breadCrumbList.length
+        }
+    }
+}
