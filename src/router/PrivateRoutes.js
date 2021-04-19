832e3e3bdaf0258b09ac7d51ec5cd0fdf11f53c2
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-19 16:51:46 +0200

    Improve Router.

--- src/router/PrivateRoutes.js
+++ src/router/PrivateRoutes.js
@@ -1,2 +1,32 @@
+// Import Modules
+const dashboard = () => import('@/modules/Dashboard')
+const users = () => import('@/modules/Users')
+
 // Export
-export default []
+export default [
+
+    // Dashboard
+    {
+        path: '/admin/dashboard',
+        component: dashboard,
+        name: 'admin.dashboard',
+        meta: {
+            breadCrumb: [
+                { name: 'Dashboard', link: 'admin.dashboard' },
+            ]
+        }
+    },
+
+    // Users
+    {
+        path: '/admin/users',
+        component: users,
+        name: 'admin.users',
+        meta: {
+            breadCrumb: [
+                { name: 'Users', link: 'admin.users' }
+            ]
+        }
+    },
+
+]
--- src/router/PublicRoutes.js
+++ src/router/PublicRoutes.js
@@ -1,18 +1,13 @@
 // Import Modules
-const dashboard = () => import('@/modules/Dashboard');
+// 
 
 // Export
 export default [
+
     // Home
     {
         path: '/',
-        redirect: '/dashboard' 
-    },
-
-    // Dashboard
-    {
-        path: '/dashboard',
-        component: dashboard,
-        name: 'dashboard'
+        redirect: '/admin/dashboard' 
     }
+
 ]
