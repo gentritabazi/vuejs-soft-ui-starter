56ee66e6459a5cbba9e7e9a73db773e8b22e0be3
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-20 00:34:49 +0200

    Add components.

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -1,3 +1,13 @@
+import { mapGetters } from 'vuex'
+import loaderComponent from '@/components/Loader'
+import notificationComponent from '@/components/Notification'
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: { loaderComponent, notificationComponent },
+    computed: {
+        ...mapGetters({
+            displayLoader: 'getDisplayLoader'
+        })
+    }
 }
