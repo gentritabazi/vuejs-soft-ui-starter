db04bf2a63fffe3adca994f0947d5881744f91b3
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-19 15:53:56 +0200

    Improve Sidenav Component.

--- src/components/Sidenav/Sidenav.js
+++ src/components/Sidenav/Sidenav.js
@@ -1,3 +1,10 @@
+import { appConfig } from '@/config/app.js'
+
 export default {
-    name: 'Sidenav'
+    name: 'Sidenav',
+    data() {
+        return {
+            appName: appConfig.APP_NAME
+        }
+    },
 }
