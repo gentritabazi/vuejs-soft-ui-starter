f90d65754babc32f8d1ec183cb90a20dc7045dfc
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-20 00:35:05 +0200

    Add func for Login.

--- src/modules/Login/Login.js
+++ src/modules/Login/Login.js
@@ -1,8 +1,30 @@
 import guestLayout from '@/layouts/Guest'
+import httpAxios from '@/utils/http-axios'
 
 export default {
     name: 'Login',
+    data() {
+        return {
+            email: null,
+            password: null
+        }
+    },
     components: {
         guestLayout
+    },
+    methods: {
+        login() {
+            const self = this
+
+            httpAxios({
+                url: '/login',
+                method: 'POST',
+                data: { email: self.email, password: self.password }
+            }).then(async response => {
+                self.$store.commit('LOGGED_USER', response.data)
+
+                self.$router.go({ name: 'admin.dashboard' })
+            })
+        }
     }
 }
