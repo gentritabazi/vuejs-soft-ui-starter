52c066190bfb13e8bb26be0d59b3ba8403a7bb20
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-20 00:33:47 +0200

    Install some packages.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './modules/App/index.vue'
 import router from './router'
+import store from './store'
+import VueNotificationList from '@dafcoe/vue-notification'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).use(VueNotificationList).mount('#app')
