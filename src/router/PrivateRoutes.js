d433edccc76a5fe36f023e359a65da62bfb03770
Gentrit Abazi <35135482+gentritabazi01@users.noreply.github.com>
2021-07-05 10:28:40 +0200

    Update PrivateRoutes.js

--- src/router/PrivateRoutes.js
+++ src/router/PrivateRoutes.js
@@ -11,7 +11,7 @@ export default [
         component: dashboard,
         name: 'admin.dashboard',
         meta: {
-            auth: true,
+            // auth: true,
             breadCrumb: [
                 { name: 'Dashboard', link: 'admin.dashboard' },
             ]
